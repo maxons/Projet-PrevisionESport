b1a5a643470acc50a7fc84d3451b03894197f1f3
maxons <orymaxence@gmail.com>
2017-01-25 21:24:45 +0100

    On garde game_id valable
    On retire les game_id pour lesquels on a des données manquantes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import scipy
 import sklearn as sk
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 # Importation des donnees
 game_train = pd.read_csv("ML_TEST/game_teams_train.csv")
@@ -15,7 +16,23 @@ game_test = pd.read_csv("ML_TEST/game_teams_test.csv")
 player_test = pd.read_csv("ML_TEST/game_player_teams_test.csv")
 
 # On verifie que les donnees ont bien ete importees
-print game_train.shape
-print game_test.shape
-print player_train.shape
-print player_test.shape
+print(game_train.shape)
+print(game_test.shape)
+print(player_train.shape)
+print(player_test.shape)
+
+# Mis en forme des donnees
+# - f/t -> 0/1, on garde les NaN
+colGame = ['first_blood', 'first_tower', 'first_inhibitor', 'first_baron', 'first_dragon']
+colPlayer = ['first_blood_kill', 'first_blood_assist', 'first_tower_kill', 'first_tower_assist', 'first_inhibitor_kill', 'first_inhibitor_assist']
+
+game_train[colGame]
+player_train[colPlayer]
+
+temp = game_train[colGame]
+ind = np.where(temp == 'f')
+
+#--------------
+# On souhaite faire les predictions uniquement avec les donnees propres aux matchs (pour le moment)
+#--------------
+
