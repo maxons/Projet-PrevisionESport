12e3aea0bbe32180c2408e70a1c6f7bebc2dec60
maxons <orymaxence@gmail.com>
2017-01-26 17:16:34 +0100

    Optimisation de la procédure
    On optimise un paramètre du random forest pour mieux prédire

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ X_train_pca = pca.fit(X_train_pca).transform(X_train_pca)
 plt.plot(pca.explained_variance_ratio_)
 plt.show()
 
-# On en choisit 3 (explication de 90% de la variance)
+# On en choisit 2 (explication de 90% de la variance)
 
 pca = PCA(n_components = 3)
 X_train_pca = pca.fit(X_train_pca).transform(X_train_pca)
@@ -64,12 +64,12 @@ plt.show()
 color = y_train
 
 plt.figure(1)
-plt.subplot(311)
+plt.subplot(211)
 plt.scatter(X_train_pca[:,0],X_train_pca[:,1], s = 5,
          cmap = "jet", c=color)
 plt.title("ACP")
 
-plt.subplot(312)
+plt.subplot(212)
 plt.scatter(X_train_pca[:,1],X_train_pca[:,2], s = 5,
          cmap = "jet", c=color)
 plt.title("ACP")
@@ -112,12 +112,31 @@ for ii in range(0,18):
 plt.plot(res)
 plt.show()
 
+# On voit que le mieux est obtenu pour 2 composantes
+# On admet que la meilleure solution est obtenue sans ACP. On va chercher à optimiser l'algorithme
 
+from sklearn.model_selection import GridSearchCV
+param = [{"max_features":list(range(2,15))}]
+best_feat = GridSearchCV(RandomForestClassifier(n_estimators=100),param,cv=5,n_jobs=-1)
+best_feat = best_feat.fit(X_train, y_train)
+# paramètre optimal
+best_feat.best_params_
 
+forest = RandomForestClassifier(n_estimators=500, criterion='gini', max_depth=None, 
+	min_samples_split=2, min_samples_leaf=1, max_features=5, max_leaf_nodes=None, bootstrap=True, oob_score=True)
 
+# Apprentissage
+forest = forest.fit(X_train,y_train)
+print(1-forest.oob_score_)
+# erreur de prévision sur le test
+1-forest.score(X_test,y_test)
+#0.0336
 
-
-
+# Prevision
+y_chap = forest.predict(X_test)
+# matrice de confusion
+table=pd.crosstab(y_test, y_chap)
+print(table)
 
 
 
