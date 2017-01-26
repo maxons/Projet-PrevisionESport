2d64b71ec7b88f781d687c597acbadafb9f0baca
maxons <orymaxence@gmail.com>
2017-01-26 15:37:32 +0100

    Fonction automatisant fusion par somme des deux jeux
    Implémentation de la fonction prepareData qui permet de mettre automatiquement les données en forme, jusqu'à l'ACP, en donnant uniquement le nom des deux fichiers à traiter

--- functions_data.py
+++ functions_data.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 #------------
 # Fonctions qui permettent de mettre en place les données
 #------------
@@ -21,5 +23,60 @@ def replaceTFplayer (data):
 	for col in colPlayer:
 		data[col].replace(['t','f'], [1,0], inplace = True)
 	return(data)
+
+def prepareData (file_game, file_players):
+	import pandas as pd
+	import numpy as np
+	import sklearn as sk
+	from sklearn.decomposition import PCA
+	from sklearn.preprocessing import Imputer
+	game = pd.read_csv(file_game)
+	player = pd.read_csv(file_players)
+	# Elements que l'on veut jeter - variable choisie au hasard
+	bool =  pd.isnull(game['first_blood'])
+	# On garde en memoire les game_id pour les retirer aussi de l'autre jeu
+	game_to_remove = np.unique(game['game_id'][bool])
+	# On ne garde que les game_id juges valable
+	game = game[:][~bool]
+	# Maintenant on veut retirer ces matchs du jeu de donnees avec les joueurs
+	# On va commencer par trier les donnees par game_id - team_id - player_id
+	player.sort_values(by = ['game_id', 'team_id', 'player_id'], inplace = True)
+	# Indices des matchs a retirer
+	bool = pd.Series(player['game_id']).isin(game_to_remove)
+	player = player[:][~bool]
+	# On retourne sur game.
+	# On veut remplacer créer une nouvelle variable valant 1 si l'équipe a gg, 0 sinon
+	victory = (game['winner_id'] == game['team_id'])*1
+	game['victory'] = victory
+	# On veut remplacer tous les T/F par 1/0 dans les deux dataset
+	game = replaceTFgame(game)
+	player = replaceTFplayer(player)
+	# Grace a notre façon de trier, on a juste a faire la somme de cinq elements a la suite pour avoir la somme par equipe
+	player_temp = player.drop(['game_id', 'team_id', 'player_id', 'champion_id','first_blood_kill',
+	 'first_blood_assist', 'first_tower_kill', 'first_tower_assist', 'first_inhibitor_kill', 'first_inhibitor_assist'], 1)
+	# On retire les valeurs manquantes de 'killing_sprees' et 'double_kills' en les estimant par la moyenne
+	imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+	player_temp = imp.fit_transform(player_temp)
+	n = int(player_temp.shape[0]/5)
+	m = player_temp.shape[1]
+	# Matrice qui va contenir toutes les sommes
+	res = np.arange(n*m).reshape(n, m)
+	# On calcule les sommes
+	for ii in range(0, n):
+		res[ii:(ii+1)] = np.array(player_temp[5*ii:5*(ii+1)].sum(0)).reshape(1,m)
+	res = pd.DataFrame(res)
+	# On range la matrice obtenu dans game
+	n = game.shape[0]
+	game.index = (range(0, n))
+	game = game.join(res)
+	# ACP
+	dataset = game.drop(['game_id', 'winner_id', 'team_id', 'victory'], 1)
+	pca = PCA()
+	X = pca.fit(dataset).transform(dataset)
+	y = game['victory']
+	return X, y
+
+
+
 
 
--- main.py
+++ main.py
@@ -9,138 +9,31 @@ import matplotlib.pyplot as plt
 import math
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import Imputer
+import functions_data as fd
+import functions_data as fd
+from sklearn.ensemble import RandomForestClassifier
 
+game_train_f = "ML_TEST/game_teams_train.csv"
+player_train_f = "ML_TEST/game_player_teams_train.csv"
 
-# Importation des donnees
-game_train = pd.read_csv("ML_TEST/game_teams_train.csv")
-player_train = pd.read_csv("ML_TEST/game_player_teams_train.csv")
-
-game_test = pd.read_csv("ML_TEST/game_teams_test.csv")
-player_test = pd.read_csv("ML_TEST/game_player_teams_test.csv")
-
-# On verifie que les donnees ont bien ete importees
-print(game_train.shape)
-print(game_test.shape)
-print(player_train.shape)
-print(player_test.shape)
-
-#------------
-# Rapide mise en forme des donnees
-#------------
-
-# Dans game_train: on remarque que pour certains matchs on a aucune information, on veut les supprimer
-# Rem: soit on a tout, soit on a rien. On va chercher pour une variable seulement.
-
-# Elements que l'on veut jeter
-bool =  pd.isnull(game_train['first_blood'])
-# On garde en memoire les game_id pour les retirer aussi de l'autre jeu
-game_to_remove = np.unique(game_train['game_id'][bool])
-# On ne garde que les game_id juges valable
-game_train = game_train[:][~bool]
-
-# On retire 271 matchs sur 1864, soit 15% des données. Cela fait beaucoup de pertes...
-
-# Maintenant on veut retirer ces matchs du jeu de donnees avec les joueurs
-# On va commencer par trier les donnees par game_id - team_id - player_id
-player_train.sort_values(by = ['game_id', 'team_id', 'player_id'], inplace = True)
-# Indices des matchs a retirer
-bool = pd.Series(player_train['game_id']).isin(game_to_remove)
-player_train = player_train[:][~bool]
-
-# On retourne sur game_train.
-# On veut remplacer créer une nouvelle variable valant 1 si l'équipe a gg, 0 sinon
-victory = (game_train['winner_id'] == game_train['team_id'])*1
-game_train['victory'] = victory
-
-# On veut remplacer tous les T/F par 1/0 dans les deux dataset
-game_train = replaceTFgame(game_train)
-player_train = replaceTFplayer(player_train)
-
-
-#------------
-# Fusion des deux jeux de donnees
-#------------
-
-# On souhaite fusionner les deux jeux de donnees afin d'avoir plus d'informations pour faire nos predictions.
-# Le but est de sommer les variables qui peuvent l'etre dans le jeu de donnees jouer/joueur pour en faire des variables par equipe
-# On va perdre certaines informations, comme le champion utilisé par chaque joueur, mais dans ce cas ce n'est pas tres grave car
-# on retrouve en general le meme type de champion dans chaque lane.
-
-
-# Grace a notre façon de trier, on a juste a faire la somme de cinq elements a la suite pour avoir la somme par equipe
-player_temp = player_train.drop(['game_id', 'team_id', 'player_id', 'champion_id','first_blood_kill',
- 'first_blood_assist', 'first_tower_kill', 'first_tower_assist', 'first_inhibitor_kill', 'first_inhibitor_assist'], 1)
-
-# On retire les valeurs manquantes de 'killing_sprees' et 'double_kills' en les estimant par la moyenne
-imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-player_temp = imp.fit_transform(player_temp)
-
-n = int(player_temp.shape[0]/5)
-m = player_temp.shape[1]
-
-# Matrice qui va contenir toutes les sommes
-res = np.arange(n*m).reshape(n, m)
-
-# On calcule les sommes
-for ii in range(0, n):
-	res[ii:(ii+1)] = np.array(player_temp[5*ii:5*(ii+1)].sum(0)).reshape(1,m)
-
-res = pd.DataFrame(res)
-
-# On range la matrice obtenu dans game_train
-n = game_train.shape[0]
-game_train.index = (range(0, n))
-game_train = game_train.join(res)
-
-# On veut savoir si des variables contiennent des NaN
-test = game_train.apply(np.isnan)
-for col in test.columns:
-	print(np.unique(test[col]))
-
-
-#------------
-# Première analyse
-#------------
-
-# On fait quelques histogrammes
-
-plt.hist(game_train[0], bins = 12)
-plt.title("Distribution des kill")
-plt.show()
-
-plt.hist(game_train[1], bins = 12)
-plt.title("Distribution des death")
-plt.show()
-
-plt.hist(game_train[2], bins = 12)
-plt.title("Distribution des assists")
-plt.show()
-
-
-# ACP
-
-dataset = game_train.drop(['game_id', 'winner_id', 'team_id', 'victory'], 1)
-pca = PCA()
-X_reduced = pca.fit(dataset).transform(dataset)
-X_train = X_reduced
-y_train = dataset['victory']
+X_train, y_train = fd.prepareData(game_train_f, player_train_f)
 
 # Graphique
-color = [dataset['victory']]
+color = y_train
 
 plt.figure(1)
 plt.subplot(311)
-plt.scatter(X_reduced[:,0],X_reduced[:,1], s = 5,
+plt.scatter(X_train[:,0],X_train[:,1], s = 5,
          cmap = "jet", c=color)
 plt.title("ACP")
 
 plt.subplot(312)
-plt.scatter(X_reduced[:,1],X_reduced[:,2], s = 5,
+plt.scatter(X_train[:,1],X_train[:,2], s = 5,
          cmap = "jet", c=color)
 plt.title("ACP")
 
 plt.subplot(313)
-plt.scatter(X_reduced[:,2],X_reduced[:,3], s = 5,
+plt.scatter(X_train[:,2],X_train[:,3], s = 5,
          cmap = "jet", c=color)
 plt.title("ACP")
 
@@ -148,9 +41,6 @@ plt.show()
 
 
 # Random forest
-
-from sklearn.ensemble import RandomForestClassifier
-
 forest = RandomForestClassifier(n_estimators=500, criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, 
 	max_features='auto', max_leaf_nodes=None, bootstrap=True, oob_score=True)
 
