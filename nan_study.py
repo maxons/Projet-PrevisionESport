cf327ea303937d8352c2a62246e3bf14e89f1cd7
maxons <orymaxence@gmail.com>
2017-01-27 13:36:24 +0100

    Recherches des variables impliquées par valeurs manquantes
    On calcule pour chaque individu le nombre de valeurs manquantes. On identifie plusieurs classes. Pour chacune de ces classes on regarde les variables concernées.

--- nan_study.py
+++ nan_study.py
@@ -25,10 +25,10 @@ def nan_per_ind (data):
 nan_game = nan_per_ind(game)
 nan_player = nan_per_ind(player)
 
-pd.crosstab(index = nan_game[0], columns = "lol")
-pd.crosstab(index = nan_player[0], columns = "lol")
-
-
+pd.crosstab(index = nan_game[0], columns = "nbLines")
+# 9 variables impliquées ou rien
+pd.crosstab(index = nan_player[0], columns = "nbLines")
+# 1-2-3-39 ou 40 variables
 
 plt.figure(1)
 plt.subplot(211)
@@ -36,4 +36,97 @@ plt.hist(nan_game)
 plt.subplot(212)
 plt.hist(nan_player)
 plt.show()
+
+# On veut savoir pour chacun des cas quelles sont les variables qui sont touchees
+# Donnees des matches
+nb_nan = (game.apply(np.isnan)*1).apply(sum,0)
+
+# Donnees des joueurs
+nb_nan = (player.apply(np.isnan)*1).apply(sum,1)
+
+nan_1 = np.where(nb_nan == 1)[0]
+col_nan_1 = (player.loc[nan_1].apply(np.isnan)*1).apply(sum, 0)
+
+
+nan_2 = np.where(nb_nan == 2)[0]
+col_nan_2 = (player.loc[nan_2].apply(np.isnan)*1).apply(sum, 0)
+
+
+nan_3 = np.where(nb_nan == 3)[0]
+col_nan_3 = (player.loc[nan_3].apply(np.isnan)*1).apply(sum, 0)
+
+
+nan_39 = np.where(nb_nan == 39)[0]
+col_nan_39 = (player.loc[nan_39].apply(np.isnan)*1).apply(sum, 0)
+
+
+nan_40 = np.where(nb_nan == 40)[0]
+col_nan_40 = (player.loc[nan_40].apply(np.isnan)*1).apply(sum, 0)
+
+
+
+
+col_nan_1[np.where(col_nan_1 > 0)[0]]
+col_nan_2[np.where(col_nan_2 > 0)[0]]
+col_nan_3[np.where(col_nan_3 > 0)[0]]
+col_nan_39[np.where(col_nan_39 > 0)[0]]
+col_nan_40[np.where(col_nan_40 > 0)[0]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
