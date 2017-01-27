175dcc38b59d74f6d575e8302389fa799dc10461
maxons <orymaxence@gmail.com>
2017-01-27 13:39:33 +0100

    On commente le code
    Le code du commit précédent n'était pas commenté...

--- nan_study.py
+++ nan_study.py
@@ -42,30 +42,28 @@ plt.show()
 nb_nan = (game.apply(np.isnan)*1).apply(sum,0)
 
 # Donnees des joueurs
+# Individu par individu, le nombre de valeurs manquantes
 nb_nan = (player.apply(np.isnan)*1).apply(sum,1)
 
+# On regarde pour chacune des classes obtenues les variables impliquees
+# Indices de(s) individu(s) ou on a x valeurs manquantes
 nan_1 = np.where(nb_nan == 1)[0]
+# Somme sur chaque variable pour savoir combien de fois la variable est impliquee pour x valeurs manquantes
 col_nan_1 = (player.loc[nan_1].apply(np.isnan)*1).apply(sum, 0)
-
 
 nan_2 = np.where(nb_nan == 2)[0]
 col_nan_2 = (player.loc[nan_2].apply(np.isnan)*1).apply(sum, 0)
 
-
 nan_3 = np.where(nb_nan == 3)[0]
 col_nan_3 = (player.loc[nan_3].apply(np.isnan)*1).apply(sum, 0)
-
 
 nan_39 = np.where(nb_nan == 39)[0]
 col_nan_39 = (player.loc[nan_39].apply(np.isnan)*1).apply(sum, 0)
 
-
 nan_40 = np.where(nb_nan == 40)[0]
 col_nan_40 = (player.loc[nan_40].apply(np.isnan)*1).apply(sum, 0)
 
-
-
-
+# On regarde uniquement les variables impliquees pour x valeurs manquantes
 col_nan_1[np.where(col_nan_1 > 0)[0]]
 col_nan_2[np.where(col_nan_2 > 0)[0]]
 col_nan_3[np.where(col_nan_3 > 0)[0]]
